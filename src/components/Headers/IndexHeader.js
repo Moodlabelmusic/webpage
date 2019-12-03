5b75b6a4bb6a7eb87c10dc67ee89985141982549
Miguel Montañez <miguel.montanez@bizagi.com>
2019-12-03 12:31:10 -0500

    refactoring template

---
+++ src/components/Headers/IndexHeader.js
@@ -0,0 +1,67 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+
+// core components
+
+function IndexHeader() {
+  return (
+    <>
+      <div
+        className="page-header section-dark"
+        style={{
+          backgroundImage:
+            "url(" + require("assets/img/header1.jpg") + ")"
+        }}
+      >
+        <div className="filter" />
+        <div className="content-center">
+          <Container>
+            <div className="title-brand">
+              {/* <h1 className="presentation-title">Paper Kit React</h1> */}
+              <img
+                alt="..."
+                className="creative-tim-logo"
+                src={require("assets/img/mood-label-music-blanco.png")}
+              />
+              {/* <div className="fog-low">
+                <img alt="..." src={require("assets/img/fog-low.png")} />
+              </div>
+              <div className="fog-low right">
+                <img alt="..." src={require("assets/img/fog-low.png")} />
+              </div> */}
+            </div>
+          </Container>
+        </div>
+        {/* <div
+          className="moving-clouds"
+          style={{
+            backgroundImage: "url(" + require("assets/img/clouds.png") + ")"
+          }}
+        /> */}
+      </div>
+    </>
+  );
+}
+
+export default IndexHeader;

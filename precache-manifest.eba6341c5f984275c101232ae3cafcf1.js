a6d2ce97f5d7e1818d02a842b5595cad06a72e50
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-04 22:23:03 +0000

    Deploy MinMiguelM/moodlabelmusic to github.com/MinMiguelM/moodlabelmusic.git:gh-pages

---
+++ precache-manifest.eba6341c5f984275c101232ae3cafcf1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44f1a9725fccb72e75f22b9dc65c59e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06f8565e08ea6ebd9561",
+    "url": "./static/css/main.d005085b.chunk.css"
+  },
+  {
+    "revision": "a9bbba870d0521b24c26",
+    "url": "./static/js/2.9083e486.chunk.js"
+  },
+  {
+    "revision": "06f8565e08ea6ebd9561",
+    "url": "./static/js/main.7d4fafab.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "11d20ea03fe1a6f7840dff2aaf49519b",
+    "url": "./static/media/header1.11d20ea0.jpg"
+  },
+  {
+    "revision": "f24e86d45f4a47199d0e6ec22d816e61",
+    "url": "./static/media/mood-label-music-blanco.f24e86d4.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "./static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "./static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "./static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "./static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7aa388cae1077f2c34ba3446e87fccd5.js"
+  "./precache-manifest.eba6341c5f984275c101232ae3cafcf1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });

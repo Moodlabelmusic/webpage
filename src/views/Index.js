adb7824f1a23aa278a9604fcbf71a89825599dae
MinMiguelM <cmiguelmg@gmail.com>
2019-12-07 18:03:55 -0500

    adding player to each track

--- src/views/Index.js
+++ src/views/Index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import SC from "soundcloud";
+
 import NavbarComponent from "components/Navbar.js";
 import Header from "components/Header.js";
 import Footer from "components/Footer.js";
@@ -6,8 +8,15 @@ import Footer from "components/Footer.js";
 // index sections
 import SectionContactUs from "./index-sections/SectionContactUs";
 import SectionAboutUs from "./index-sections/SectionAboutUs";
+import SectionCatalog from "./index-sections/SectionCatalog";
 
 function Index() {
+  const clientId = "Vu5tlmvC9eCLFZkxXG32N1yQMfDSAPAA";
+  SC.initialize({
+    client_id: clientId,
+    redirect_uri: ""
+  });
+
   document.documentElement.classList.remove("nav-open");
   React.useEffect(() => {
     document.body.classList.add("index");
@@ -20,6 +29,7 @@ function Index() {
       <NavbarComponent />
       <Header />
       <div className="main">
+        <SectionCatalog />
         <SectionAboutUs />
         <SectionContactUs />
         <Footer />
